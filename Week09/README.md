88a9e76c9a1672fbe9c79f8eab8f009879112145
iamsharleen <iamsharleen@outlook.com>
2021-01-31 22:36:31 +0800

    week09 homework

--- Week09/README.md
+++ Week09/README.md
@@ -1,1 +1,107 @@
-学习笔记
+# 学习笔记
+
+## HTML解析
+### 语法分析
+1. HTML parse模块的文件拆分
+2. 用FSM实现HTML分析
+3. 解析标签
+    * 标签：
+        * 开始标签
+        * 结束标签
+        * 自封闭标签
+4. 创建元素
+5. 处理属性
+### 语法分析
+6. 用token构建DOM树
+    *栈
+7. 将文本节点加到DOM树
+
+## CSS计算
+1. 收集CSS规则
+2. 添加调用
+    * 创建一个元素后立即计算CSS
+    * 分析一个元素时，所有CSS规则已收集完毕
+    * 真实浏览器中可能遇到写在body中的style，需重新计算CSS（忽略）
+3. 获取父元素序列
+    * 必须知道元素的所有父元素才能判断元素是否与规则匹配
+    * 从stack中首先获取的是当前元素，所以计算父元素匹配的顺序是从内到外
+    ```
+    //slice: stack元素不断变化，可能会污染。slice用于截取数组，不会影响原数组
+    //reverse: 标签匹配会从当前元素开始逐级往外匹配
+    var elements = stack.slice().reverse();
+    ```
+4. 选择器与元素的匹配
+5. 计算选择器与元素匹配
+6. 生成computed属性
+7. specificity的计算逻辑
+    * id > class > tagName
+    ```
+    [0,0,0,0] = [inline,id,class,tag]
+    
+    e.g.
+    a: div div #id => [0,1,0,2]
+    b: div #my #id =>[0,2,0,1]
+    c: div #id => [0,1,0,1]
+    b: div .class #id => [0,1,1,1]
+    
+    Result:
+    b>a
+    a>c
+    d>a
+
+    ```
+
+## 查漏补缺
+### CSS选择器
+1. Universal
+    * \*
+2. Type
+    * E
+3. Attribute
+    * E[foo]
+    * E[foo="bar"]
+    * E[foo~="bar"]
+    * E[foo^="bar"]
+    * E[foo$="bar"]
+    * E[foo*="bar"]
+    * E[foo|="en"]
+    * E:root
+3. Pseudo-classes
+    * E:nth-child(n)
+    * E:nth-last-child(n)
+    * E:nth-of-type(n)
+    * E:nth-last-of-type(n)
+    * E:first-child
+    * E:last-child
+    * E:first-of-type
+    * E:last-of-type
+    * E:only-of-type
+    * E:empty
+    * E:link
+    * E:visited
+    * E:active
+    * E:hover
+    * E:focus
+    * E:target
+    * E:lang(fr)
+    * E:enabled
+    * E:disabled
+    * E:checked
+    * E:not(s)
+4. pseudo-element
+    * E::first-line
+    * E::first-letter
+    * E::before
+    * E::after
+5. Class
+    * E.warning
+6. ID
+    * E#myid
+7. Combinator
+    * E F
+    * E>F
+    * E+F
+    * E~F
+    
+## TODO
+支持CSS复杂选择器
---
+++ Week09/client.js
@@ -0,0 +1,253 @@
+
+const net = require("net");
+const parser = require("./parser.js");
+
+class Request {
+  constructor(options) {
+    this.method = options.method || "Get";
+    this.host = options.host;
+    this.port = options.port || 80;
+    this.path = options.path || "/";
+    this.body = options.body || {};
+    this.headers = options.headers || {};
+    if (!this.headers['Content-Type']) {
+      this.headers['Content-Type'] = "application/x-www-form-urlencoded";
+    }
+
+    if (this.headers['Content-Type'] === "application/json") {
+      this.bodyText = JSON.stringify(this.body);
+    } else if (this.headers['Content-Type'] === "application/x-www-form-urlencoded") {
+      this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('$');
+    }
+
+    this.headers["Content-Length"] = this.bodyText.length;
+  }
+
+  /**
+   * 在Request的构造器中收集必要信息
+   * 设计一个send函数，把请求发送到服务器
+   * 异步的
+   * @param {*} connection 
+   */
+  send(connection) {
+    return new Promise((resolve, reject) => {
+      const parser = new RespnseParser;
+
+      if (connection) {
+        connection.write(this.toString());
+      } else {
+        console.log(this.host, this.port);
+        connection = net.createConnection({
+          host: this.host,
+          port: this.port
+        }, () => {
+          console.log(this.toString());
+          connection.write(this.toString());
+        })
+      }
+      connection.on('data', (data) => {
+        console.log("Data--> ", data.toString());
+        parser.receive(data.toString());
+        if (parser.isFinished) {
+          resolve(parser.response);
+          connection.end();
+        }
+      });
+      connection.on('error', (err) => {
+        console.log("Error--> ");
+        reject(err)
+        connection.end();
+      })
+    })
+  }
+
+  toString() {
+    //不能有缩进
+    return `POST / HTTP/1.1\r
+${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r
+\r
+${this.bodyText}`
+
+    //     return `POST / HTTP/1.1\r
+    // X-Foo2: customed\r
+    // Content-Type: application/x-www-form-urlencoded\r
+    // Content-Length: 11\r
+    // \r
+    // name = winter`;
+  }
+
+}
+
+
+class RespnseParser {
+  constructor() {
+    //状态机
+    //TODO 改成函数写法
+    this.WAITTING_STATUS_LINE = 0;
+    this.WAITTING_STATUS_LINE_END = 1;
+    this.WAITTING_HEADER_NAME = 2;
+    this.WAITTING_HEADER_SPACE = 3;
+    this.WAITTING_HEADER_VALUE = 4;
+    this.WAITTING_HEADER_LINE_END = 5;
+    this.WAITTING_HEADER_BLOCK_END = 6;
+    this.WAITTING_BODY = 7;
+
+    this.current = this.WAITTING_STATUS_LINE;
+    this.statusLine = "";
+    this.headers = {};
+    this.headerName = "";
+    this.headerValue = "";
+    this.bodyParser = null;
+
+  }
+  receive(string) {
+    // console.log("Rceived string-->\r\n", string);
+    // console.log("------>END")
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i));
+    }
+    // console.log("statusLine --> ", this.statusLine);
+    // console.log("headers --> ", this.headers);
+    // console.log("headerName --> ", this.headerName);
+    // console.log("headerValue --> ", this.headerValue);
+    // console.log("bodyParser --> ", this.bodyParser);
+  }
+
+  get isFinished() {
+    return this.bodyParser && this.bodyParser.isFinished;
+  }
+
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      headers: this.headers,
+      body: this.bodyParser.content.join('')
+    }
+  }
+
+  /**
+   * 用状态机解析字符串
+   */
+  receiveChar(c) {
+    if (this.current === this.WAITTING_STATUS_LINE) {
+      if (c === '\r') {
+        this.current = this.WAITTING_STATUS_LINE_END;
+      } else {
+        this.statusLine += c;
+      }
+    } else if (this.current === this.WAITTING_STATUS_LINE_END) {
+      if (c === '\n') {
+        this.current = this.WAITTING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITTING_HEADER_NAME) {
+      if (c === ':') {
+        this.current = this.WAITTING_HEADER_SPACE;
+      } else if (c === '\r') {
+        this.current = this.WAITTING_HEADER_BLOCK_END;
+        if (this.headers['Transfer-Encoding'] === 'chunked') {
+          this.bodyParser = new TrunkedBodyParser();
+        }
+      } else {
+        this.headerName += c;
+      }
+    } else if (this.current === this.WAITTING_HEADER_SPACE) {
+      if (c === ' ') {
+        this.current = this.WAITTING_HEADER_VALUE;
+      }
+    } else if (this.current === this.WAITTING_HEADER_VALUE) {
+      if (c === '\r') {
+        this.current = this.WAITTING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = "";
+        this.headerValue = "";
+      } else {
+        this.headerValue += c;
+      }
+    } else if (this.current === this.WAITTING_HEADER_LINE_END) {
+      if (c === '\n') {
+        this.current = this.WAITTING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITTING_HEADER_BLOCK_END) {
+      if (c === '\n') {
+        this.current = this.WAITTING_BODY;
+      }
+    } else if (this.current === this.WAITTING_BODY) {
+      // console.log(c)
+      this.bodyParser.receiveChar(c);
+    }
+  }
+}
+
+class TrunkedBodyParser {
+  constructor() {
+    this.WAITTING_LENGTH = 0;
+    this.WAITTING_LENGTH_LINE_END = 1;
+    this.READING_TRUNK = 2;
+    this.WAITTING_NEW_LINE = 3;
+    this.WAITTING_NEW_LINE_END = 4;
+    this.length = 0;
+    this.content = [];
+    this.isFinished = false;
+
+    this.current = this.WAITTING_LENGTH;
+  }
+
+
+  receiveChar(char) {
+    if (this.current === this.WAITTING_LENGTH) {
+      if (char === '\r') {
+        if (this.length === 0) {
+          this.isFinished = true;
+        }
+        this.current = this.WAITTING_LENGTH_LINE_END;
+      } else {
+        this.length *= 16;
+        this.length += parseInt(char, 16);
+      }
+    } else if (this.current === this.WAITTING_LENGTH_LINE_END) {
+      if (char === '\n') {
+        this.current = this.READING_TRUNK;
+      }
+    } else if (this.current === this.READING_TRUNK) {
+      this.content.push(char);
+      this.length--;
+      if (this.length === 0) {
+        this.current = this.WAITTING_NEW_LINE;
+      }
+    } else if (this.current === this.WAITTING_NEW_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITTING_NEW_LINE_END;
+      }
+    } else if (this.current === this.WAITTING_NEW_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITTING_LENGTH;
+      }
+    }
+  }
+}
+
+void async function () {
+  let request = new Request({
+    method: "POST",
+    host: "127.0.0.1",
+    port: "8080",
+    path: "/",
+    headers: {
+      ["X-Foo2"]: "customed"
+    },
+    body: {
+      name: "winter"
+    }
+
+  })
+
+  let response = await request.send();
+  //console.log(response);
+  //实际上应该采用异步分段处理
+  let dom = parser.parseHTML(response.body);
+
+}();
+
+
